358dd858cdf453e2c3f2e10539e48e33cce181db
Agustlescano <agustinlescano37@gmail.com>
2021-10-13 22:07:04 -0300

    ultima tarea jsii

--- 02-JSII/homework/homework.js
+++ 02-JSII/homework/homework.js
@@ -3,6 +3,7 @@
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+return array[array.length -1];
 
 }
 
@@ -10,6 +11,10 @@ function incrementarPorUno(array) {
   // "array" debe ser una arreglo de números enteros
   // Aumenta cada número por 1 y devuelve el array
   // Tu código:
+for (var i=0; i<array.length; i++){
+array[i] = array[i] + 1;
+}
+return array ;
 
 }
 
@@ -17,7 +22,8 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
-
+array.push (elemento);
+return array;
 }
 
 function dePalabrasAFrase(palabras) {
@@ -26,13 +32,21 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
-
+var oracion = palabras[0];
+for (var i=1 ; i < palabras.length ; i++){
+oracion = oracion + ' ' + palabras[i];
+}
+  return oracion;
+ 
 }
 
 function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+for (var i=0 ; i < array.length ; i++){
+  if (array[i]===elemento){return true}
+  }   return false;
 
 }
 
@@ -40,13 +54,26 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser un array de enteros
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+var total=0;
+for ( var i=0; i < resultadosTest.length; i++){
+  total= total + resultadosTest[i];
 
+}
+return ( total / resultadosTest.length)
 }
 
 function numeroMasGrande(numeros) {
   // "numeros" debe ser un array de enteros
   // Devuelve el número más grande
   // Tu código:
+var numeromayor= 0;
+for (var i = 0 ; i < numeros.length; i++){
+  if (numeromayor < numeros[i]){
+     numeromayor = numeros[i];
+  }
+  
+}
+return numeromayor;
 
 }
 
